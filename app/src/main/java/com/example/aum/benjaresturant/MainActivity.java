776ae9c200dadb8d64c226a29302327be1524114
Doraemon <tanaboon.c@gmail.com>
2016-02-29 10:51:08 +0700

    test

---
+++ app/src/main/java/com/example/aum/benjaresturant/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.aum.benjaresturant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onC
+
+    reate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/aum/benjaresturant/MyOpenHelper.java
@@ -0,0 +1,41 @@
+package com.example.aum.benjaresturant;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by aum on 2/29/2016.
+ */
+public class MyOpenHelper extends SQLiteOpenHelper{
+    //Explicit
+    public static final String database_name = "benja.db";
+    private static final int database_version = 1;
+    private static final String create_table_user = "create table userTABLE (" +
+            "_id integer primary key, " +
+            "User text, " +
+            "Password text, " +
+            "Name text);";
+    private static final String create_table_food = "create table foodTABLE (" +
+            "_id integer primary key, " +
+            "Food text, " +
+            "Price text, " +
+            "Source text);";
+
+    public MyOpenHelper(Context context) {
+        super(context, database_name, null,database_version);
+
+    } // Constructor
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_table_food);
+        sqLiteDatabase.execSQL(create_table_user);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+} // Main Class
